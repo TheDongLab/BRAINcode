ecbfd999c84eaab1ed07d3a11413f552b0a9b96c
Zach Wolfe <64483133+xcwolfe@users.noreply.github.com>
2026-01-08 15:40:18 -0500

    Refactor SNP detection script for clarity and paths that match _RNAseq.sh

--- modules/_callSNP.sh
+++ modules/_callSNP.sh
@@ -6,22 +6,23 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=80G
 
-####################################
-## Detect SNP/indels from RNA-seq BAM/SAM
-## Updated to use genome folder: /home/zw529/donglab/pipelines/genome
-####################################
+###########################################
+# Detect SNP/indels from RNA-seq BAM/SAM
+# Updated for local genome and rnaseq modules
+###########################################
+
+set -e
 
 if [ $# -ne 1 ]; then
-    echo "Usage: `basename $0` <input.sam or .bam>"
-    exit
+    echo "Usage: $(basename $0) <input.sam or .bam>"
+    exit 1
 fi
 
-input_sam=$1
-pipeline_path=/home/zw529/donglab/pipelines/modules/rnaseq
-export PATH=$pipeline_path:$PATH
+input_sam="$1"
+pipeline_path=/home/zw529/donglab/pipelines/scripts/rnaseq
+module_path=/home/zw529/donglab/pipelines/scripts/rnaseq
 
 GENOME_DIR=/home/zw529/donglab/pipelines/genome
-
 dbSNP=$GENOME_DIR/dbSNP.bed
 rRNA=$GENOME_DIR/rRNA.bed
 LINE=$GENOME_DIR/LINE.bed
@@ -29,59 +30,66 @@ exons=$GENOME_DIR/exons.bed
 introns=$GENOME_DIR/introns.bed
 
 # convert BAM to SAM if necessary
-if [[ $input_sam == *.bam ]]; then
-    samtools view -h -o ${input_sam%.bam}.sam $input_sam
-    input_sam=${input_sam%.bam}.sam
+if [[ "$input_sam" == *.bam ]]; then
+    samtools view -h -o "${input_sam%.bam}.sam" "$input_sam"
+    input_sam="${input_sam%.bam}.sam"
 fi
 
-# Split large files
-size=$(ls -sH $input_sam | cut -f1 -d' ')
-echo "File size is $size KB"
+# Split large files if >10 GB
+size_kb=$(ls -sH "$input_sam" | awk '{print $1}')
+echo "File size is $size_kb KB"
 
-if [ "$size" -gt 10000000 ]; then
-    split -b 1000M $input_sam tmp_sampiece_
+cd "$(dirname "$input_sam")"
+bam_base=$(basename "$input_sam" .sam)
+
+if [ "$size_kb" -gt 10000000 ]; then
+    split -b 1000M "$input_sam" tmp_sampiece_
     > .paraFile
     for i in tmp_sampiece_*; do
-        echo "awk -f $pipeline_path/modules/_sam2variation.awk $i > tmp_snp.$i" >> .paraFile
+        echo "awk -f $module_path/_sam2variation.awk $i > tmp_snp.$i" >> .paraFile
     done
     rm -f .paraFile.completed
     ParaFly -c .paraFile -CPU 8
-
-    cat tmp_snp* | sort -u > ${input_sam/sam/snp}
-    rm tmp_snp* tmp_sampiece_*
+    cat tmp_snp* | sort -u > "${bam_base}.snp"
+    rm -f tmp_snp* tmp_sampiece_*
 else
-    awk -f $pipeline_path/modules/_sam2variation.awk $input_sam | sort -u > ${input_sam/sam/snp}
+    awk -f "$module_path/_sam2variation.awk" "$input_sam" | sort -u > "${bam_base}.snp"
 fi
 
 # SNP position histogram
-textHistogram -col=4 -maxBinCount=100 ${input_sam/sam/snp} > ${input_sam/sam/snp}.relpos.hist
+textHistogram -col=4 -maxBinCount=100 "${bam_base}.snp" > "${bam_base}.snp.relpos.hist"
 
 # get read length
-readslength=$(grep -v -m 10 "^@" $input_sam | awk '{print length($10)}' | sort -nr | head -n1)
+readslength=$(awk 'BEGIN{max=0} !/^@/{len=length($10); if(len>max) max=len} END{print max}' "$input_sam")
 
 # filter SNPs near read ends
-awk -v RL=$readslength '$4>=10 && $4<=(RL-10)' ${input_sam/sam/snp} \
+awk -v RL=$readslength '$4>=10 && $4<=(RL-10)' "${bam_base}.snp" \
     | cut -f1-2 \
     | sed 's/:/\t/;s/-/\t/' \
     | bedtools groupby -g 1-4 -c 4 -o count \
-    | sort -k5,5nr > ${input_sam/sam/snp.depth}
+    | sort -k5,5nr > "${bam_base}.snp.depth"
 
-textHistogram -col=5 -minVal=10 -maxBinCount=100 -binSize=10 ${input_sam/sam/snp.depth} > ${input_sam/sam/snp.depth.hist
+textHistogram -col=5 -minVal=10 -maxBinCount=100 -binSize=10 "${bam_base}.snp.depth" > "${bam_base}.snp.depth.hist"
 
 # retain SNPs with depth >=16
-awk '$5>15' ${input_sam/sam/snp}.depth | sortBed > ${input_sam/sam/snp}.depth_gt_15
+awk '$5>15' "${bam_base}.snp.depth" | sortBed > "${bam_base}.snp.depth_gt_15"
 
 # annotate SNPs
-intersectBed -a ${input_sam/sam/snp}.depth_gt_15 -b $dbSNP   -sorted -wao | cut -f1-5,9 | groupBy -g 1-5 -c 6 -o collapse > ${input_sam/sam/snp}_dbSNP
-intersectBed -a ${input_sam/sam/snp}.depth_gt_15 -b $rRNA    -sorted -wao | cut -f1-5,9 | groupBy -g 1-5 -c 6 -o collapse > ${input_sam/sam/snp}_rRNA
-intersectBed -a ${input_sam/sam/snp}.depth_gt_15 -b $LINE    -sorted -wao | cut -f1-5,9 | groupBy -g 1-5 -c 6 -o collapse > ${input_sam/sam/snp}_LINE
-intersectBed -a ${input_sam/sam/snp}.depth_gt_15 -b $exons   -sorted -wao | cut -f1-5,9 | uniq | groupBy -g 1-5 -c 6 -o collapse > ${input_sam/sam/snp}_exon
-intersectBed -a ${input_sam/sam/snp}.depth_gt_15 -b $introns -sorted -wao | cut -f1-5,9 | uniq | groupBy -g 1-5 -c 6 -o collapse > ${input_sam/sam/snp}_intron
+intersectBed -a "${bam_base}.snp.depth_gt_15" -b "$dbSNP"   -sorted -wao | cut -f1-5,9 | groupBy -g 1-5 -c 6 -o collapse > "${bam_base}.snp_dbSNP"
+intersectBed -a "${bam_base}.snp.depth_gt_15" -b "$rRNA"    -sorted -wao | cut -f1-5,9 | groupBy -g 1-5 -c 6 -o collapse > "${bam_base}.snp_rRNA"
+intersectBed -a "${bam_base}.snp.depth_gt_15" -b "$LINE"    -sorted -wao | cut -f1-5,9 | groupBy -g 1-5 -c 6 -o collapse > "${bam_base}.snp_LINE"
+intersectBed -a "${bam_base}.snp.depth_gt_15" -b "$exons"   -sorted -wao | cut -f1-5,9 | uniq | groupBy -g 1-5 -c 6 -o collapse > "${bam_base}.snp_exon"
+intersectBed -a "${bam_base}.snp.depth_gt_15" -b "$introns" -sorted -wao | cut -f1-5,9 | uniq | groupBy -g 1-5 -c 6 -o collapse > "${bam_base}.snp_intron"
 
-# combine annotations in order: dbSNP, exon, intron, LINE, rRNA
-paste ${input_sam/sam/snp}_* | awk '{OFS="\t"; printf "%s\t%s\t%s\t%s\t%s",$1,$2,$3,$4,$5; for(i=6;i<=NF;i+=6) printf "\t%s",$i; printf "\n"}' > ${input_sam/sam/snp}.annotation
+# combine annotations: dbSNP, exon, intron, LINE, rRNA
+paste "${bam_base}.snp_"* | awk '{
+    OFS="\t";
+    printf "%s\t%s\t%s\t%s\t%s",$1,$2,$3,$4,$5;
+    for(i=6;i<=NF;i+=6) printf "\t%s",$i;
+    printf "\n"
+}' > "${bam_base}.snp.annotation"
 
-# cleanup
-rm ${input_sam/sam/snp}_*
+# cleanup intermediate annotation files
+rm -f "${bam_base}.snp_"*
 
-echo "SNP annotation complete: ${input_sam/sam/snp}.annotation"
+echo "SNP annotation complete: ${bam_base}.snp.annotation"
