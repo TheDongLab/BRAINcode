301c5d01273de620d7831a524a345a1e34caba89
Zach Wolfe <64483133+xcwolfe@users.noreply.github.com>
2026-01-08 15:37:07 -0500

    Update RNAseq script for version 2.2

--- modules/_RNAseq.sh
+++ modules/_RNAseq.sh
@@ -1,96 +1,84 @@
-###########################################
-# bash script for running paired-end RNAseq
-# author: Xianjun Dong and Zachery Wolfe (modified)
-# email: xdong@rics.bwh.harvard.edu
-# date: 1/8/2026
-# version: 2.1
-# Note: call this script in the folder containing raw FASTQ files (R1.fastq.gz / R2.fastq.gz)
-###########################################
 #!/bin/bash
-
 ###########################################
-echo "["`date`"] STEP 0. define paths & inputs"
+# RNAseq pipeline (paired-end)
+# Author: Xianjun Dong & Zachery Wolfe
+# Date: 1/8/2026
+# Version: 2.2
 ###########################################
 
-# Paths to binaries and pipeline scripts
-BIN_DIR=~/donglab/pipelines/modules/rnaseq/bin
-SCRIPT_DIR=~/donglab/pipelines/scripts/rnaseq
+echo "[`date`] STEP 0. define paths & inputs"
 
-outputdir="$3"      # third argument from run_RNAseq.txt
-samplename=$(basename "$R1" | sed 's/_1\.fastq\.gz//')  # extract sample name from FASTQ1
-CPU=8               # or pass as arg
+# Third argument: output folder
+outputdir="$3"
+mkdir -p "$outputdir"
 
-# Genome references (using initialized STAR hg38)
+# Number of threads
+CPU=${CPU:-4}
+
+# Sample name inferred from folder name
+samplename=$(basename "$outputdir")
+
+# Input FASTQ (must be full paths)
+R1="$1"
+R2="$2"
+
+# Paths
+SCRIPT_DIR=$(dirname "$0")
 GENOME_DIR=/home/zw529/donglab/pipelines/genome
 GENOME_FASTA=$GENOME_DIR/hg38.fa
 GENOME_STAR_INDEX=$GENOME_DIR
 GENOME_GTF=$GENOME_DIR/hg38.ncbiRefSeq.gtf
 GENOME_REF_FLAT=$GENOME_DIR/refFlat.txt
-GENOME_BED12=""
+GENOME_BED12=$GENOME_DIR/hg38.bed12
+CHROM_INFO=$GENOME_DIR/ChromInfo.txt
+CHR_M_BED=$GENOME_DIR/chrM.bed
+MASK_GTF=""  # optional
 
-# Optional mask GTF for cufflinks
-MASK_GTF=""
-
-# STAR mapping options
 MAX_HIT=10
-strandoption=""  # set "--library-type fr-firststrand" for stranded data
+strandoption=""
 
-# Input FASTQ (assumes running inside folder containing R1/R2)
-R1=$(ls *_1.fastq.gz | head -n1)
-R2=$(ls *_2.fastq.gz | head -n1)
-
-# Sample and output names
-samplename=${PWD##*/}        # folder name as sample name
-inputdir=$PWD
-outputdir=$inputdir/../processed
-modulename="RNAseq"
-
-# Make output folder if it doesn't exist
-mkdir -p $outputdir/$samplename
+mkdir -p "$outputdir/$samplename"
 
 ###########################################
-echo "["`date`"] STEP 1. quality check (FastQC)"
+echo "[`date`] STEP 1. quality check (FastQC)"
 ###########################################
-
 module load FastQC
-[ ! -f $outputdir/$samplename/.status.$modulename.fastqc ] && \
-fastqc -t $CPU -o $outputdir/$samplename $R1 $R2 && \
-touch $outputdir/$samplename/.status.$modulename.fastqc
+
+[ ! -f "$outputdir/$samplename/.status.RNAseq.fastqc" ] && \
+fastqc -t $CPU --nogroup -o "$outputdir/$samplename" "$R1" "$R2" && \
+touch "$outputdir/$samplename/.status.RNAseq.fastqc"
 
 ###########################################
-echo "["`date`"] STEP 2. adaptor/quality trimming (Trim Galore)"
+echo "[`date`] STEP 2. adaptor/quality trimming (Trim Galore)"
 ###########################################
+module load Trim_Galore
 
-module load TrimGalore
+[ ! -f "$outputdir/$samplename/.status.RNAseq.trim" ] && \
+trim_galore --paired --cores $CPU --fastqc --output_dir "$outputdir/$samplename" "$R1" "$R2" && \
+touch "$outputdir/$samplename/.status.RNAseq.trim"
 
-[ ! -f $outputdir/$samplename/.status.$modulename.trim ] && \
-trim_galore --paired --cores $CPU --fastqc --output_dir $outputdir/$samplename $R1 $R2 && \
-touch $outputdir/$samplename/.status.$modulename.trim
-
-# Update R1/R2 to trimmed files
-R1=$outputdir/$samplename/$(basename $R1 .fastq.gz)_val_1.fq.gz
-R2=$outputdir/$samplename/$(basename $R2 .fastq.gz)_val_2.fq.gz
-
-###########################################
-echo "["`date`"] STEP 3. optional kPAL QC / library complexity"
-###########################################
-
-[ ! -f $outputdir/$samplename/.status.$modulename.kpal ] && \
-kpal -f $R1 $R2 -o $outputdir/$samplename/kpal_results && \
-touch $outputdir/$samplename/.status.$modulename.kpal
+# Update R1/R2 to trimmed outputs
+R1="$outputdir/$samplename/$(basename $R1 .fastq.gz)_val_1.fq.gz"
+R2="$outputdir/$samplename/$(basename $R2 .fastq.gz)_val_2.fq.gz"
 
 ###########################################
-echo "["`date`"] STEP 4. mapping with STAR"
+echo "[`date`] STEP 3. optional kPAL QC / library complexity"
 ###########################################
+[ ! -f "$outputdir/$samplename/.status.RNAseq.kpal" ] && \
+kpal count "$R1" "$R2" -o "$outputdir/$samplename/kpal_results" && \
+touch "$outputdir/$samplename/.status.RNAseq.kpal"
 
+###########################################
+echo "[`date`] STEP 4. mapping with STAR"
+###########################################
 module load STAR
 
-[ ! -f $outputdir/$samplename/.status.$modulename.mapping ] && \
+[ ! -f "$outputdir/$samplename/.status.RNAseq.mapping" ] && \
 STAR --runThreadN $CPU \
      --genomeDir $GENOME_STAR_INDEX \
-     --readFilesIn $R1 $R2 \
+     --readFilesIn "$R1" "$R2" \
      --readFilesCommand zcat \
-     --outFileNamePrefix $outputdir/$samplename/ \
+     --outFileNamePrefix "$outputdir/$samplename/" \
      --outSAMtype BAM SortedByCoordinate \
      --outSAMattrRGline ID:$samplename SM:$samplename LB:$samplename PL:ILLUMINA PU:$samplename \
      --outFilterMultimapNmax $MAX_HIT \
@@ -99,74 +87,72 @@ STAR --runThreadN $CPU \
      --chimJunctionOverhangMin 10 \
      --chimOutType Junctions \
      --outSAMstrandField intronMotif && \
-touch $outputdir/$samplename/.status.$modulename.mapping
+touch "$outputdir/$samplename/.status.RNAseq.mapping"
 
 ###########################################
-echo "["`date`"] STEP 4.1. circRNA calling with CIRCexplorer2"
+echo "[`date`] STEP 4.1. circRNA calling with CIRCexplorer2"
 ###########################################
-
 module load CIRCexplorer2
 
-cd $outputdir/$samplename
+cd "$outputdir/$samplename"
 
-[ ! -f .status.$modulename.circRNA ] && \
-CIRCexplorer2 parse -t STAR \
-     -b Chimeric.out.junction \
-     > .CIRCexplorer2_parse.log && \
-CIRCexplorer2 annotate -r $GENOME_REF_FLAT \
-     -g $GENOME_FASTA \
-     -b back_spliced_junction.bed \
-     -o circularRNA_known.txt --low-confidence \
-     > .CIRCexplorer2_annotate.log && \
-touch .status.$modulename.circRNA
+[ ! -f .status.RNAseq.circRNA ] && \
+CIRCexplorer2 parse -t STAR -b Chimeric.out.junction > .CIRCexplorer2_parse.log && \
+CIRCexplorer2 annotate -r $GENOME_REF_FLAT -g $GENOME_FASTA -b back_spliced_junction.bed \
+     -o circularRNA_known.txt --low-confidence > .CIRCexplorer2_annotate.log && \
+touch .status.RNAseq.circRNA
 
-[ ! -f .status.$modulename.circRNA.circexplore.denovo ] && \
-CIRCexplorer2 assemble -r $GENOME_REF_FLAT -m . -o assemble -p $CPU --remove-rRNA > .CIRCexplorer_assemble.log && \
-CIRCexplorer2 denovo -r $GENOME_REF_FLAT --as=AS --abs=ABS \
-     -g $GENOME_FASTA \
-     -b back_spliced_junction.bed -d assemble -m . -o denovo_circ \
-     > .CIRCexplorer_denovo.log && \
-touch .status.$modulename.circRNA.circexplore.denovo
+[ ! -f .status.RNAseq.circRNA.circexplore.denovo ] && \
+CIRCexplorer2 assemble -r $GENOME_REF_FLAT -m . -o assemble -p $CPU --remove-rRNA > .CIRCexplorer2_assemble.log && \
+CIRCexplorer2 denovo -r $GENOME_REF_FLAT --as=AS --abs=ABS -g $GENOME_FASTA \
+     -b back_spliced_junction.bed -d assemble -m . -o denovo_circ > .CIRCexplorer2_denovo.log && \
+touch .status.RNAseq.circRNA.circexplore.denovo
 
 ###########################################
-echo "["`date`"] STEP 5. post-processing, indexing, stats, bigwig"
+echo "[`date`] STEP 5. post-processing, indexing, stats, bigwig"
 ###########################################
 
-cd $outputdir/$samplename
+cd "$outputdir/$samplename"
 
 # index STAR BAM
-[ ! -f .status.$modulename.sam2bam ] && \
+[ ! -f .status.RNAseq.sam2bam ] && \
 samtools index Aligned.sortedByCoord.out.bam && \
-touch .status.$modulename.sam2bam
+touch .status.RNAseq.sam2bam
 
 # alignment stats
-[ ! -f .status.$modulename.bam2stat ] && \
-echo `samtools view -cF 0x100 Aligned.sortedByCoord.out.bam` "primary alignments" > Aligned.sortedByCoord.out.bam.stat && \
+[ ! -f .status.RNAseq.bam2stat ] && \
+echo "$(samtools view -cF 0x100 Aligned.sortedByCoord.out.bam) primary alignments" > Aligned.sortedByCoord.out.bam.stat && \
 samtools flagstat Aligned.sortedByCoord.out.bam >> Aligned.sortedByCoord.out.bam.stat && \
-touch .status.$modulename.bam2stat
+touch .status.RNAseq.bam2stat
 
 # bam2annotation
-[ ! -f .status.$modulename.bam2annotation ] && \
-([ -f Aligned.sortedByCoord.out.bam.bam2annotation ] || $SCRIPT_DIR/_bam2annotation.sh Aligned.sortedByCoord.out.bam > Aligned.sortedByCoord.out.bam.bam2annotation) && \
-Rscript $SCRIPT_DIR/_bam2annotation.r Aligned.sortedByCoord.out.bam.bam2annotation Aligned.sortedByCoord.out.bam.bam2annotation.pdf && \
-touch .status.$modulename.bam2annotation
+[ ! -f .status.RNAseq.bam2annotation ] && \
+$SCRIPT_DIR/_bam2annotation.sh Aligned.sortedByCoord.out.bam > Aligned.sortedByCoord.out.bam.bam2annotation && \
+Rscript $SCRIPT_DIR/_bam2annotation.r Aligned.sortedByCoord.out.bam.bam2annotation \
+     Aligned.sortedByCoord.out.bam.bam2annotation.pdf && \
+touch .status.RNAseq.bam2annotation
 
-# bigwig for UCSC
-[ ! -f .status.$modulename.sam2bw ] && \
-total_mapped_reads=`grep -w total_non_rRNA_mt Aligned.sortedByCoord.out.bam.bam2annotation | cut -f2 -d' '` && \
-bam2bigwig.sh Aligned.sortedByCoord.out.bam "" $total_mapped_reads && \
-touch .status.$modulename.sam2bw
+# bigwig
+[ ! -f .status.RNAseq.sam2bw ] && \
+total_mapped_reads=$(grep -w total_non_rRNA_mt Aligned.sortedByCoord.out.bam.bam2annotation | cut -f2 -d' ') && \
+$SCRIPT_DIR/bam2bigwig.sh Aligned.sortedByCoord.out.bam "" $total_mapped_reads && \
+touch .status.RNAseq.sam2bw
 
 # SNP calling
-[ ! -f .status.$modulename.callSNP ] && \
+[ ! -f .status.RNAseq.callSNP ] && \
 $SCRIPT_DIR/_callSNP.sh Aligned.sortedByCoord.out.bam && \
-touch .status.$modulename.callSNP
+touch .status.RNAseq.callSNP
 
-# assembly/quantification with cufflinks and htseq-count
-[ ! -f .status.$modulename.cufflinks ] && \
-cufflinks --no-update-check $strandoption -o ./ -p $CPU -G $GENOME_GTF -M $MASK_GTF --compatible-hits-norm --multi-read-correct Aligned.sortedByCoord.out.bam && \
-touch .status.$modulename.cufflinks
+# assembly/quantification with cufflinks
+[ ! -f .status.RNAseq.cufflinks ] && \
+cufflinks --no-update-check $strandoption -o ./ -p $CPU -G $GENOME_GTF -M $MASK_GTF \
+     --compatible-hits-norm --multi-read-correct Aligned.sortedByCoord.out.bam && \
+touch .status.RNAseq.cufflinks
 
-[ ! -f .status.$modulename.htseqcount ] && \
-htseq-count -m intersection-strict -t exon -i gene_id -s no -q -f bam Aligned.sortedByCoord.out.bam $GENOME_GTF > hgseqcount.by.gene.tab 2> hgseqcount.by.gene.tab.stderr && \
-touch .status.$modulename.htseqcount
+# gene counting
+[ ! -f .status.RNAseq.htseqcount ] && \
+htseq-count -m intersection-strict -t exon -i gene_id -s no -q -f bam \
+     Aligned.sortedByCoord.out.bam $GENOME_GTF > hgseqcount.by.gene.tab 2> hgseqcount.by.gene.tab.stderr && \
+touch .status.RNAseq.htseqcount
+
+echo "[`date`] RNAseq pipeline finished for $samplename"
