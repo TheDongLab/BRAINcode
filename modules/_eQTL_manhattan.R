315b7ab3b70cb713d9b19195ec1209878428edbc
Zach Wolfe <64483133+xcwolfe@users.noreply.github.com>
2026-04-10 14:11:22 -0400

    Update _eQTL_manhattan.R
    Fixed "build_cumulative" function error (relating to data.table)

--- modules/_eQTL_manhattan.R
+++ modules/_eQTL_manhattan.R
@@ -23,8 +23,8 @@ suppressPackageStartupMessages({
 })
 
 args       <- commandArgs(TRUE)
-eqtl_file  <- args[1]   # full_annotated.txt
-lead_file  <- args[2]   # lead_snps.txt
+eqtl_file  <- args[1]
+lead_file  <- args[2]
 out_prefix <- args[3]
 fdr_thresh <- ifelse(is.na(args[4]), 0.05, as.numeric(args[4]))
 
@@ -35,13 +35,13 @@ lead <- fread(lead_file)[chr %in% paste0("chr", 1:22)]
 eqtl[, chr_num := as.integer(sub("chr","", chr))]
 lead[, chr_num := as.integer(sub("chr","", chr))]
 
-# Calculate p-value cutoff for the FDR line
 fdr_pval_cutoff <- max(eqtl[FDR < fdr_thresh]$`p-value`, na.rm=TRUE)
 
-# Function to build cumulative coordinates for the x-axis
+# Fixed build_cumulative function
 build_cumulative <- function(dt, pos_col) {
     dt <- copy(dt)
-    setorder(dt, chr_num, get(pos_col))
+    # Use setorderv for dynamic column names
+    setorderv(dt, c("chr_num", pos_col))
     chr_max <- dt[, .(max_pos = max(get(pos_col), na.rm=TRUE)), by=chr_num]
     chr_max[, offset := cumsum(shift(max_pos, fill=0))]
     dt <- merge(dt, chr_max[, .(chr_num, offset)], by="chr_num")
@@ -55,36 +55,32 @@ snp_build <- build_cumulative(eqtl[!is.na(pos)], "pos")
 snp_data  <- snp_build$data
 snp_axis  <- snp_build$axis
 
-# Downsample non-significant points to keep PDF size manageable
-set.seed(1234)
+set.seed(42)
 snp_sig    <- snp_data[`p-value` <= fdr_pval_cutoff]
 snp_nonsig <- snp_data[`p-value` > fdr_pval_cutoff & `p-value` <= 0.001]
 snp_nonsig <- snp_nonsig[sample(.N, min(.N, 150000))]
 snp_plot   <- rbind(snp_sig, snp_nonsig)
 snp_plot[, log10p := -log10(`p-value`)]
 
-# Prepare lead SNPs for diamond shapes
 lead_plot <- build_cumulative(lead[!is.na(pos)], "pos")$data
 lead_plot[, log10p := -log10(`p-value`)]
 
-# Identify extreme hits for "Needle" labels (-log10p > 100)
+# Extreme hits for "Needle" labels (-log10p > 100)
 extreme_hits <- snp_plot[log10p > 100]
 
 message("## Creating Plot...")
 p1 <- ggplot(snp_plot, aes(x=cum_pos, y=log10p)) +
-    # Normal and Telomeric points
     geom_point(data=snp_plot[!telomeric_flag], aes(colour=as.character(chr_num %% 2)), size=0.6, alpha=0.7) +
     geom_point(data=snp_plot[telomeric_flag == TRUE], colour="#E69F00", size=0.6, alpha=0.9) +
-    # Lead SNPs (Diamonds)
     geom_point(data=lead_plot, aes(x=cum_pos, y=log10p), shape=18, size=2, colour="red") +
-    # FDR Significance Line
     geom_hline(yintercept=-log10(fdr_pval_cutoff), linetype="dashed", colour="red", linewidth=0.5) +
-    # NEEDLE LABELS for extreme hits
-    geom_segment(data=extreme_hits, aes(x=cum_pos, xend=cum_pos, y=log10p, yend=log10p + 15), 
-                 colour="darkgreen", linetype="dotted") +
-    geom_text_repel(data=extreme_hits, aes(x=cum_pos, y=log10p + 15, label=geneid), 
-                    size=3, colour="darkgreen", fontface="bold", box.padding = 0.5) +
-    # Scales and Theme
+    
+    # Needles starting from y=0
+    geom_segment(data=extreme_hits, aes(x=cum_pos, xend=cum_pos, y=0, yend=log10p + 5), 
+                 colour="darkgreen", linetype="dotted", linewidth=0.4) +
+    geom_text_repel(data=extreme_hits, aes(x=cum_pos, y=log10p + 5, label=geneid), 
+                    size=3, colour="darkgreen", fontface="italic", box.padding = 0.5) +
+
     scale_colour_manual(values=c("0"="#555555", "1"="#AAAAAA")) +
     scale_x_continuous(labels=paste0("chr", snp_axis$chr_num), breaks=snp_axis$centre, expand=c(0.01,0.01)) +
     scale_y_continuous(expand=c(0.01, 0.1)) +
