7b616b55cffbf04a2e4d4f541e5d104fb0e6b194
Zach Wolfe <64483133+xcwolfe@users.noreply.github.com>
2026-04-28 15:53:58 -0500

    Made awk less aggressive so that we aren't printing ..'s, which bcftools can't parse

--- modules/split_vcf_by_chr.sh
+++ modules/split_vcf_by_chr.sh
@@ -19,7 +19,6 @@ METADATA="/home/zw529/donglab/data/target_ALS/targetALS_rnaseq_metadata.csv"
 mkdir -p $OUTPUT_DIR
 
 # --- STEP 1: GENERATE CLEAN MALE LIST ---
-# We only care about males because they are the ones causing "ambiguous" errors on ChrX
 echo "Generating male sample list..."
 tr -d '\r' < $METADATA | awk -F',' 'NR>1 && $2 != "" && (tolower($5) == "male") {print $2}' | \
 sed 's/^[[:space:]]*//;s/[[:space:]]*$//' | sort | uniq > ${OUTPUT_DIR}/males.txt
@@ -35,8 +34,9 @@ for chr in {1..22} X Y; do
         # 1. Get header
         bcftools view -r chrX $INPUT_VCF -h > ${OUTPUT_DIR}/header.txt
         
-        # 2. Re-code male genotypes to be haploid (strip / and |)
-        # This awk script finds which columns are male and removes the slashes from their genotypes
+        # 2. Re-code male genotypes safely
+        # We check if the genotype is missing (contains .) - if so, we leave it as .
+        # If not, we strip the slash to make it haploid.
         bcftools view -r chrX $INPUT_VCF -H | \
         awk -v males="$(cat ${OUTPUT_DIR}/males.txt | tr '\n' ',')" -v head="$(grep "^#CHROM" ${OUTPUT_DIR}/header.txt)" '
         BEGIN {
@@ -48,7 +48,10 @@ for chr in {1..22} X Y; do
         }
         {
             for (i=10; i<=NF; i++) {
-                if (male_col[i]) gsub(/[\/|]/, "", $i);
+                if (male_col[i]) {
+                    if ($i ~ /\./) { $i = "." } # Handle missing data correctly
+                    else { gsub(/[\/|]/, "", $i) } # Strip slashes for actual genotypes
+                }
             }
             print $0;
         }' | cat ${OUTPUT_DIR}/header.txt - | bcftools view -Oz -o $OUT_VCF
@@ -59,7 +62,6 @@ for chr in {1..22} X Y; do
         bcftools view -r chr${chr} $INPUT_VCF -Oz -o $OUT_VCF
     fi
     
-    # -f forces index overwrite
     bcftools index -f -t $OUT_VCF
 done
 
