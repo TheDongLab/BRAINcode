87e2d04d4ed88713aa982e24e992a2274e00184b
Zach Wolfe <64483133+xcwolfe@users.noreply.github.com>
2026-05-13 14:31:51 -0400

    Change script to extract first 20 female subjects
    TOPMed requires minimum 20 samples for imputation

--- modules/extract_first_20_female_subjects.sh
+++ modules/extract_first_20_female_subjects.sh
@@ -5,16 +5,16 @@ VCF_IN="/home/zw529/donglab/data/target_ALS/QTL/joint_genotyped_GQ.vcf.gz"
 FEMALES_SRC="/home/zw529/donglab/data/target_ALS/QTL/chromosome_joint_vcfs/females.txt"
 REF="/home/zw529/donglab/references/genome/Homo_sapiens/UCSC/hg38/Sequence/WholeGenomeFasta/genome.fa"
 OUT_DIR="/home/zw529/donglab/data/target_ALS/QTL/diagnostics"
-OUT_VCF="${OUT_DIR}/TargetALS_chrX_first5_Females.vcf.gz"
+OUT_VCF="${OUT_DIR}/TargetALS_chrX_first20_Females.vcf.gz"
 
 module load BCFtools/1.21
 
-# 1. Create a temporary list of the first 5 female IDs
-head -n 5 "$FEMALES_SRC" > "${OUT_DIR}/top5_female_ids.txt"
+# 1. Create a temporary list of the first 20 female IDs
+head -n 20 "$FEMALES_SRC" > "${OUT_DIR}/top20_female_ids.txt"
 
 # 2. Extract, Normalize, and Zip
 # We include 'chrX' to limit the scope and 'norm' to ensure the VCF is clean
-bcftools view -S "${OUT_DIR}/top5_female_ids.txt" "$VCF_IN" chrX | \
+bcftools view -S "${OUT_DIR}/top20_female_ids.txt" "$VCF_IN" chrX | \
 bcftools norm -m -any -f "$REF" | \
 bcftools sort -Oz -o "$OUT_VCF"
 
