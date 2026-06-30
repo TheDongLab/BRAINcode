b0d230fe55d964a7a49d57a57fb983f5c2b9152e
Zach Wolfe <64483133+xcwolfe@users.noreply.github.com>
2026-06-30 11:11:01 -0400

    Updated the function to check for 'geneid' in addition to 'gene' and 'gene_id'

--- modules/convert_eqtl_names.R
+++ modules/convert_eqtl_names.R
@@ -24,8 +24,10 @@ convert_eqtl_genes <- function(file_path, output_path) {
   cat("Processing:", basename(file_path), "\n")
   data_table <- read.table(file_path, sep="\t", header=TRUE, stringsAsFactors=FALSE, check.names=FALSE)
   
-  # Identify Ensembl ID column dynamically
-  if ("gene" %in% colnames(data_table)) {
+  # Identify Ensembl ID column (added 'geneid' to the check)
+  if ("geneid" %in% colnames(data_table)) {
+    ensembl_ids <- data_table$geneid
+  } else if ("gene" %in% colnames(data_table)) {
     ensembl_ids <- data_table$gene
   } else if ("gene_id" %in% colnames(data_table)) {
     ensembl_ids <- data_table$gene_id
@@ -33,7 +35,7 @@ convert_eqtl_genes <- function(file_path, output_path) {
     ensembl_ids <- rownames(data_table)
   }
   
-  # Strip version decimals (e.g., ENSG00000123456.12 -> ENSG00000123456)
+  # Strip version decimals (e.g., ENSG00000001460.19 -> ENSG00000001460)
   ensembl_ids_clean <- gsub("\\..*$", "", ensembl_ids)
   
   # Query AnnotationHub
@@ -41,7 +43,7 @@ convert_eqtl_genes <- function(file_path, output_path) {
   human_org <- query(ah, c("OrgDb", "Homo sapiens"))
   orgdb <- ah[[names(human_org)[1]]]
   
-  # Map names
+  # Map names safely
   gene_symbols <- mapIds(
     orgdb, 
     keys = ensembl_ids_clean, 
@@ -50,10 +52,11 @@ convert_eqtl_genes <- function(file_path, output_path) {
     multiVals = "first"
   )
   
+  # Handle NAs and uniqueness
   final_names <- ifelse(is.na(gene_symbols), ensembl_ids, gene_symbols)
   final_names <- make.unique(final_names)
   
-  # Inject column
+  # Inject column at the front
   data_table$gene_symbol <- final_names
   col_order <- c("gene_symbol", colnames(data_table)[colnames(data_table) != "gene_symbol"])
   data_table <- data_table[, col_order]
